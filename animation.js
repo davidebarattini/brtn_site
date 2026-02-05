d3fd27d4d1f1ee8acb700e220ea320e42722973c
davidebarattini <davi.barattini2002@gmail.com>
2026-02-05 11:58:34 +0100

    05/02

--- animation.js
+++ animation.js
@@ -100,9 +100,11 @@ hero.appendChild(heroImagesContainer);
 // Lista immagini
 const images = [
   'img/hero_image_low.jpg',
+  'img/hero_image8_low.jpg',
   'img/hero_image2_low.jpg',
   'img/hero_image3_low.jpg',
-  'img/hero_image4_low.jpg',
+  'img/hero_image10_low.jpg',
+  'img/hero_image11_low.jpg',
   'img/hero_image6_low.jpg'
 ];
 
